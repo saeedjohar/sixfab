918354f512368feef30eee04f76c25ed076f0e3e
saeedjohar <saeed_johar@yahoo.com>
2018-01-17 13:23:04 +0300

    Initial Commit

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+
+
+setup(
+    name='sixfab',
+    version='0.1',
+    author='Saeed Johar',
+    author_email='saeed_johar@yahoo.com',
+    description='sixfab library',
+    license='MIT',
+    url='https://github.com/saeedjohar/sixfab/',
+    packages=find_packages()
+
+)
